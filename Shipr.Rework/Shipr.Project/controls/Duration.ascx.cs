fb445e3b47fe46dceadcc92da60655393dd97291
KarlKrus <carl_son_cruz@live.com>
2017-01-20 13:21:19 +0800

    01.20.17.01.21.PM

--- Shipr.Rework/Shipr.Project/controls/Duration.ascx.cs
+++ Shipr.Rework/Shipr.Project/controls/Duration.ascx.cs
@@ -9,9 +9,50 @@ namespace Shipr.Rework.Shipr.Project.controls
 {
     public partial class Duration : System.Web.UI.UserControl
     {
+        public String StartDate { get; set; }
+        public String EndDate { get; set; }
+        public String StartTime { get; set; }
+        public String EndTime { get; set; }
+        public string Title { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            ltrlTitle.Text = (Title != null) ? Title : "Duration";
 
+            if (StartDate != null && StartDate.Trim().Length > 0)
+                txtStartDate.Text = StartDate;
+            else
+                StartDate = txtStartDate.Text;
+
+            if (EndDate != null && EndDate.Trim().Length > 0)
+                txtEndDate.Text = EndDate;
+            else
+                EndDate = txtEndDate.Text;
+
+
+            if (StartTime != null && StartTime.Trim().Length > 0)
+                ddlStartTime.SelectedIndex = getSelectedIndex(ddlStartTime, StartTime);
+            else
+                StartTime = ddlStartTime.SelectedItem.Text;
+
+            if (EndTime != null && EndTime.Trim().Length > 0)
+                ddlEndTime.SelectedIndex = getSelectedIndex(ddlEndTime, EndTime);
+            else
+                EndTime = ddlEndTime.SelectedItem.Text;
+
+        }
+
+        private int getSelectedIndex(DropDownList ddl, String value)
+        {
+            for (int i = 0; i < ddl.Items.Count; i++)
+            {
+                if (ddl.Items[i].ToString().Equals(value.PadLeft(8, '0')))
+                {
+                    return i;
+                    break;
+                }
+            }
+            return 0;
         }
     }
 }
--- Shipr.Rework/Shipr.Project/controls/Duration.ascx.designer.cs
+++ Shipr.Rework/Shipr.Project/controls/Duration.ascx.designer.cs
@@ -56,5 +56,23 @@ namespace Shipr.Rework.Shipr.Project.controls {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblEndDate;
+        
+        /// <summary>
+        /// txtEndDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEndDate;
+        
+        /// <summary>
+        /// ddlEndTime control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEndTime;
     }
 }
